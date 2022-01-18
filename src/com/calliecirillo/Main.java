880724ff295b73ca654cd03f914c294cb0941a1b
Callie Warren <callie.warren@cbsinteractive.com>
2022-01-18 14:43:57 -0600

    created java hello world to test github set up

---
+++ src/com/calliecirillo/Main.java
@@ -0,0 +1,13 @@
+package com.calliecirillo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        if(args.length > 0) {
+            System.out.println("Hello number " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
