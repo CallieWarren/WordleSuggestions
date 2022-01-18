a030f77ab4150c2dba3bf445686f4ddb402281bb
Callie Warren <callie.warren@cbsinteractive.com>
2022-01-18 14:58:47 -0600

    test update

--- src/com/calliecirillo/Main.java
+++ src/com/calliecirillo/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
         if(args.length > 0) {
-            System.out.println("Hello number " + args[0]);
+            System.out.println("Hello # " + args[0]);
         } else {
             System.out.println("Hello World");
         }
